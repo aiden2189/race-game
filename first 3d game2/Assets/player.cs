288e2f9d8c14629209ac31ad757417799fa515ca
aiden2189 <58379778+aiden2189@users.noreply.github.com>
2023-08-17 00:00:01 -0500

    car made

--- first 3d game2/Assets/player.cs
+++ first 3d game2/Assets/player.cs
@@ -3,57 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
-{
-    public float speed;
-    public GameObject play;
-    public Rigidbody player1;
-    public GameObject enem;
-    public float x;
-    [SerializeField] activatepause script;
+{           
+    [SerializeField] WheelCollider FLW;
+    [SerializeField] WheelCollider FRW;
+    [SerializeField] WheelCollider RLW;
+    [SerializeField] WheelCollider RRW;
+
+    [SerializeField] Transform FLT;
+    [SerializeField] Transform FRT;
+    [SerializeField] Transform RLT;
+    [SerializeField] Transform RRT;
+
+    public float enginePower = 150;
+    public float BrakeForce = 100;
+    public float maxSteer = 25; 
+
+    public float power = 0;
+    public float brake = 0;
+    public float steer = 0;
+
     void Start()
     {
 
     }
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        //movement code
+        power = Input.GetAxis("Vertical") * enginePower;
+        steer = Input.GetAxis("Horizontal") * maxSteer;
+        brake = Input.GetKey("space") ? GetComponent<Rigidbody>().mass * 0.5f : 0;
 
-        
-        if (script.pause1 == false)
+        FLW.steerAngle = steer;
+        FRW.steerAngle = steer;
+
+        if (brake > 0)
         {
-            player1.angularVelocity = new Vector3(0, 0, 0);
-            //speed boost
-            if (Input.GetKeyDown(KeyCode.LeftShift))
-            {
-                speed = 100f;
-                x = 100f;
-            }
-
-            //actual movement code
-            if (Input.GetKey(KeyCode.W))
-            {
-                player1.AddRelativeForce(Vector3.forward * speed);
-            }
-            else if (Input.GetKey(KeyCode.S))
-            {
-                player1.AddRelativeForce(Vector3.back * speed);
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                player1.AddRelativeForce(Vector3.left * speed);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                player1.AddRelativeForce(Vector3.right * speed);
-            }
-            
+            //Brakes on 
+            FLW.brakeTorque = brake;
+            FRW.brakeTorque = brake;
+            RLW.brakeTorque = brake;
+            RRW.brakeTorque = brake;
+            //Engine idol
+            RLW.motorTorque = 0;
+            RRW.motorTorque = 0;
         }
+        else
+        {
+            //Brakes idol
+            FLW.brakeTorque = 0;
+            FRW.brakeTorque = 0;
+            RLW.brakeTorque = 0;
+            RRW.brakeTorque = 0;
+            //Engine on
+            RLW.motorTorque = power;
+            RRW.motorTorque = power;
+        }
+        UpdateWheel(FLW, FLT);
+        UpdateWheel(FRW, FRT);
+        UpdateWheel(RLW, RLT);
+        UpdateWheel(RRW, RRT);
     }
-    void OnTriggerEnter(Collider other) 
-    {  
-        if(other.tag == "enemy")
-        {
-            Destroy(play.gameObject);  
-        }
+
+    void UpdateWheel(WheelCollider col, Transform trans)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        trans.position = position;
+        trans.rotation = rotation;
     }
 }
+
+
